4d618d454bb07a84702343f0d13c0b18b077e9e4
devauradigital <devauradigital@gmail.com>
2025-11-08 22:25:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,19 @@
-const CACHE = 'dreamit-v2';
+const CACHE_NAME = 'dreamit-v1';
+const urlsToCache = [
+  '/dreamit/',
+  '/dreamit/index.html',
+  '/dreamit/site.webmanifest',
+  '/dreamit/navbarlogo.png'
+];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        '/dreamit/',
-        '/dreamit/index.html',
-        '/dreamit/site.webmanifest',
-        '/dreamit/navbarlogo.png',
-        '/dreamit/no_goal.png'
-      ]);
-    }).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
-
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(
-      caches.match('/dreamit/index.html').then(r => r || fetch(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request))
-    );
-  }
-});
-
-
-
