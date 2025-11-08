aeebaa2e8b48db1a864ee9f51762abbbc30edbce
devauradigital <devauradigital@gmail.com>
2025-11-08 21:14:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,29 @@
 // sw.js
 const CACHE = 'dreamit-v1';
-const OFFLINE_URL = '/index.html';
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
         '/',
-        OFFLINE_URL,
+        '/index.html',
+        '/site.webmanifest',
         '/navbarlogo.png',
-        '/no_goal.png',
-        '/site.webmanifest'
+        '/no_goal.png'
       ]);
     }).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(names => Promise.all(
-      names.filter(n => n !== CACHE).map(n => caches.delete(n))
-    )).then(() => self.clients.claim())
-  );
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', e => {
   if (e.request.mode === 'navigate') {
-    e.respondWith(caches.match(OFFLINE_URL).then(r => r || fetch(e.request)));
+    e.respondWith(
+      caches.match('/index.html').then(r => r || fetch(e.request))
+    );
   } else {
     e.respondWith(
       caches.match(e.request).then(r => r || fetch(e.request))
